fdfa7470823bbf39c6c990e774ab2230ca5f0954
Geovanni Santos <geovannysantos@outlook.com>
2024-03-12 11:26:08 -0300

    Primeiro_comando_"For"_executável

---
+++ soma.js
@@ -0,0 +1,10 @@
+let INDICE = 13;
+let SOMA = 0;
+// Podendo ser feito com laço de repetição for, utilizando o While
+for (let K = 1; K <= INDICE; K++) {
+
+    SOMA = SOMA + K;
+}
+
+// Console no terminal (node soma.js) vai sair o resultado da Variavel Soma.
+console.log(SOMA); // Saída: 91
